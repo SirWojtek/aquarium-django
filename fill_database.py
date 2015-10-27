66027d185e60ca095d8222cdcd69a825e6b8eca8
Okulewicz Mateusz <mateusz.okulewicz@nokia.com>
2015-10-27 17:37:09 +0100

    Added filling database automation script

---
+++ fill_database.py
@@ -0,0 +1,15 @@
+#!/usr/bin/python2
+
+from os import environ
+environ["DJANGO_SETTINGS_MODULE"] = "aquarium_django.settings"
+
+from status.models import Temperature
+import django
+
+def main():
+    for i in range(1,20):
+        t = Temperature(value = i)
+        t.save()
+
+if __name__ == '__main__':
+    main()
