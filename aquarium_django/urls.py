0949fdf02d63d13fcdfa33eb73ac2affc4cca89a
Okulewicz Mateusz <mateusz.okulewicz@nokia.com>
2015-10-30 08:44:15 +0100

    Added filter module to server

--- aquarium_django/urls.py
+++ aquarium_django/urls.py
@@ -17,10 +17,12 @@ from django.conf.urls import include, url
 from django.contrib import admin
 from stat_module import urls as stat_module_urls
 from temperature_module import urls as temperature_module_urls
+from filter_module import urls as filter_module_urls
 
 urlpatterns = [
     url(r'^admin/', include(admin.site.urls)),
     url(r'^stat/', include(stat_module_urls, namespace = 'stat_module')),
     url(r'^temperature/', include(temperature_module_urls, namespace = 'temperature_module')),
+    url(r'^filter/', include(filter_module_urls, namespace = 'filter_module')),
     url(r'^', include(stat_module_urls)),
 ]
--- temperature_module/models.py
+++ temperature_module/models.py
@@ -1,5 +1,17 @@
 from django.db import models
+from schedule.models import GenericSchedule
+from defaults import Default
 
 class TemperatureHistory(models.Model):
     value = models.IntegerField()
     timestamp = models.DateTimeField(auto_now_add = True)
+
+    def get_current_temperature(self):
+        if len(self.objects):
+            return self.objects.order_by('timestamp')[0].value
+        else:
+            return None
+
+
+class TemperatureSchedule(GenericSchedule):
+    default_value = Default.temperature
