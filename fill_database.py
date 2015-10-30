bb1ceea4ffab129651952a6a2276b6f384e1ed94
Okulewicz Mateusz <mateusz.okulewicz@nokia.com>
2015-10-30 10:10:53 +0100

    Updated fill_database script

--- fill_database.py
+++ fill_database.py
@@ -4,12 +4,17 @@ from os import environ
 environ["DJANGO_SETTINGS_MODULE"] = "aquarium_django.settings"
 
 from temperature_module.models import TemperatureHistory
+from filter_module.models import FilterHistory
 import django
 
 def main():
     for i in range(1,20):
         t = TemperatureHistory(value = i)
         t.save()
+
+    for i in [True, False, False, True]:
+        f = FilterHistory(status = i)
+        f.save()
 
 if __name__ == '__main__':
     main()
