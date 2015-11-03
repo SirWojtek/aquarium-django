b90cd9c92ca8dd8aa3a4e0c83fae4b9d64558592
Okulewicz Mateusz <mateusz.okulewicz@nokia.com>
2015-11-03 16:18:04 +0100

    Removed hardcoded module name

--- temperature_module/views.py
+++ temperature_module/views.py
@@ -1,15 +1,18 @@
 from django.shortcuts import render
 from models import TemperatureHistory, TemperatureSchedule, TemperatureScheduleForm
 
+module_name = __name__.split('.')[0]
+
 def index_get(request):
     return render(request, 'temperature/index.html', {'temp_list' : TemperatureHistory.objects.all() })
 
 def schedule_get(request):
+    print __name__
     return render(request, 'common/schedule.html', {
             'schedule_list' : TemperatureSchedule.objects.all(),
-            'add_view' : 'temperature_module:schedule_add',
-            'edit_view' : 'temperature_module:schedule_edit',
-            'delete_view' : 'temperature_module:schedule_delete',
+            'add_view' : module_name + ':schedule_add',
+            'edit_view' : module_name + ':schedule_edit',
+            'delete_view' : module_name + ':schedule_delete',
             'schedule_form' : TemperatureScheduleForm()})
 
 def schedule_add(request):
