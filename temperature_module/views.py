0bbb5afd0c0cc59b40f53ea3343dd647ace4668a
Okulewicz Mateusz <mateusz.okulewicz@nokia.com>
2015-11-03 17:02:50 +0100

    Corrected adding schedule tasks

--- temperature_module/views.py
+++ temperature_module/views.py
@@ -1,4 +1,4 @@
-from django.shortcuts import render
+from django.shortcuts import render, redirect
 from models import TemperatureHistory, TemperatureSchedule, TemperatureScheduleForm
 
 module_name = __name__.split('.')[0]
@@ -7,16 +7,21 @@ def index_get(request):
     return render(request, 'temperature/index.html', {'temp_list' : TemperatureHistory.objects.all() })
 
 def schedule_get(request):
-    print __name__
     return render(request, 'common/schedule.html', {
-            'schedule_list' : TemperatureSchedule.objects.all(),
-            'add_view' : module_name + ':schedule_add',
-            'edit_view' : module_name + ':schedule_edit',
-            'delete_view' : module_name + ':schedule_delete',
-            'schedule_form' : TemperatureScheduleForm()})
+        'schedule_list' : TemperatureSchedule.objects.all(),
+        'add_view' : module_name + ':schedule_add',
+        'edit_view' : module_name + ':schedule_edit',
+        'delete_view' : module_name + ':schedule_delete',
+        'schedule_form' : TemperatureScheduleForm() })
 
 def schedule_add(request):
-    pass
+    if not request.POST:
+        raise Exception(module_name + ".schedule_add not invoked with POST")
+    task = TemperatureScheduleForm(request.POST)
+    if not task.is_valid():
+        raise Exception(module_name + ".shedule_add form validation fails")
+    task.save()
+    return redirect(module_name + ':schedule_get')
 
 def schedule_edit(request):
     pass
