2c5edfbe7e3e7cb565698e374d7aaa514be632b3
SirWojtek <momatoku@gmail.com>
2015-11-07 20:27:56 +0100

    Added settings info stat index

--- stat_module/views.py
+++ stat_module/views.py
@@ -1,25 +1,31 @@
 from django.shortcuts import render
-from models import Status
+from models import Status, Settings
 
 def index_get(request):
-    status = Status.objects.all()
+    status = Status.get()
+    settings = Settings.get()
 
-    temp_value = 'No data'
+    temp_value_status = 'No data'
     heating_status = 'No data'
     filter_status = 'No data'
     light_status = 'No data'
-    timestamp = 'No data'
+    status_timestamp = 'No data'
 
     if status:
-        temp_value = status[0].temperature
-        heating_status = status[0].heating_status
-        filter_status = status[0].filter_status
-        light_status = status[0].light_status
-        timestamp = status[0].last_update
+        temp_value_status = status.temperature
+        heating_status = status.heating_status
+        filter_status = status.filter_status
+        light_status = status.light_status
+        status_timestamp = status.last_update
 
     return render(request, 'stat/index.html', {
-        'temp_value' : temp_value,
+        'temp_value_status' : temp_value_status,
         'heating_status' : heating_status,
         'filter_status' : filter_status,
         'light_status' : light_status,
-        'timestamp' : timestamp })
+        'status_timestamp' : status_timestamp,
+        'temp_value_settings' : settings.temperature,
+        'heating_settings' : settings.heating_status,
+        'filter_settings' : settings.filter_status,
+        'light_settings' : settings.light_status,
+        'settings_timestamp' : settings.last_update })
