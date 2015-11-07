bf9582574cc2b1c69f14d390478f7938a71d7e3a
SirWojtek <momatoku@gmail.com>
2015-11-07 20:05:08 +0100

    Added settings table and command for fill it properly

--- defaults.py
+++ defaults.py
@@ -1,4 +1,6 @@
 
 class Default:
     temperature = 25
+    heating_status = False
     filter_status = False
+    light_status = False
--- stat_module/models.py
+++ stat_module/models.py
@@ -1,5 +1,7 @@
 from django.db import models
+from defaults import Default
 
+# Contains data from driver
 class Status(models.Model):
     temperature = models.IntegerField()
     heating_status = models.BooleanField()
@@ -10,4 +12,17 @@ class Status(models.Model):
     # There should be only one record in this table
     def save(self, *args, **kwargs):
         Status.objects.all().delete()
-        super(Model, self).save(*args, **kwargs)
+        models.Model.save(self, *args, **kwargs)
+
+# Contains data for driver
+class Settings(models.Model):
+    temperature = models.IntegerField()
+    heating_status = models.BooleanField()
+    filter_status = models.BooleanField()
+    light_status = models.BooleanField()
+    last_update = models.DateTimeField(auto_now_add = True)
+
+    # There should be only one record in this table
+    def save(self, *args, **kwargs):
+        Settings.objects.all().delete()
+        models.Model.save(self, *args, **kwargs)
