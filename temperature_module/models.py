b3661a55f57c18422bb3a0170f308fb8c00af538
SirWojtek <momatoku@gmail.com>
2015-11-12 17:39:54 +0100

    Removed unused member

--- temperature_module/models.py
+++ temperature_module/models.py
@@ -1,15 +1,12 @@
 from django.db import models
 from schedule.models import GenericSchedule
 from django.forms import ModelForm
-from defaults import Default
 
 class TemperatureHistory(models.Model):
     value = models.IntegerField()
     timestamp = models.DateTimeField(auto_now_add = True)
 
 class TemperatureSchedule(GenericSchedule):
-    default_value = Default.temperature
-
     status = models.IntegerField()
 
 class TemperatureScheduleForm(ModelForm):
