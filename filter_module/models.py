e1c87b3966dc4fde77818e41df0524b9ac07c966
SirWojtek <momatoku@gmail.com>
2015-11-12 17:40:18 +0100

    Introduced schedule in filter_module

--- filter_module/models.py
+++ filter_module/models.py
@@ -1,10 +1,15 @@
 from django.db import models
 from schedule.models import GenericSchedule
-from defaults import Default
+from django.forms import ModelForm
 
 class FilterHistory(models.Model):
     status = models.BooleanField()
     timestamp = models.DateTimeField(auto_now_add = True)
 
 class FilterSchedule(GenericSchedule):
-    default_value = Default.filter_status
+    status = models.BooleanField()
+
+class FilterScheduleForm(ModelForm):
+    class Meta:
+        model = FilterSchedule
+        fields = GenericSchedule.form_fields
--- filter_module/views.py
+++ filter_module/views.py
@@ -1,5 +1,20 @@
 from django.shortcuts import render
-from models import FilterHistory
+from models import FilterHistory, FilterSchedule, FilterScheduleForm
+from schedule.views import ScheduleViews
+
+schedule_views = ScheduleViews(__name__.split('.')[0], FilterSchedule, FilterScheduleForm)
 
 def index_get(request):
     return render(request, 'filter/index.html', {'filter_status_list' : FilterHistory.objects.all() })
+
+def schedule_get(request):
+    return schedule_views.get(request)
+
+def schedule_add(request):
+    return schedule_views.add(request)
+
+def schedule_edit(request, id):
+    return schedule_views.edit(request, id)
+
+def schedule_delete(request, id):
+    return schedule_views.delete(request, id)
