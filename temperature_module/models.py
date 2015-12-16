a67c175b9d6572c66b3c2b6f61fdae69c11cdfb1
Mateusz Okulewicz <mateusz.okulewicz@gigaset.com>
2015-12-16 09:36:17 +0100

    Added dbus send

--- temperature_module/models.py
+++ temperature_module/models.py
@@ -1,22 +1,30 @@
 from django import forms
 from schedule.models import ScheduleForm, ScheduleDbusInterface
+from dbus_communication.dbus_interface import Dbus
 
 class TemperatureScheduleForm(ScheduleForm):
-	status = forms.IntegerField()
+    status = forms.IntegerField()
 
 class TemperatureDbusInterface(ScheduleDbusInterface):
     @staticmethod
     def get_schedule_list():
-        pass
+        return Dbus.get_schedule_list()
 
     @staticmethod
     def add_schedule_task(task):
-        print task
+        data = task.cleaned_data
+        task = (
+            data['start_day'], (data['start_time'].hour, data['start_time'].minute),
+            data['end_day'], (data['end_time'].hour, data['end_time'].minute),
+            data['status'])
+        Dbus.add_schedule_task(task)
 
     @staticmethod
     def update_schedule_task(old_task, new_task):
+        print "update"
         print old_task, new_task
 
     @staticmethod
     def remove_schedule_task(task):
+        print "remove"
         print task
