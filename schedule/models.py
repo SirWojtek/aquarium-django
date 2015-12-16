b1bbb98ceec002b0a2dda48157543ee57c224775
Mateusz Okulewicz <mateusz.okulewicz@gigaset.com>
2015-12-16 10:54:00 +0100

    Added presentation class for shedule task

--- schedule/models.py
+++ schedule/models.py
@@ -1,41 +1,54 @@
 from django import forms
+from datetime import time
 
-# This is abstract model used for creating schedule tables for each module.
+
+MONDAY = 'M'
+TUESDAY = 'TU'
+WEDNESDAY = 'W'
+THURSDAY = 'TH'
+FRIDAY = 'F'
+SATURDAY = 'SA'
+SUNDAY = 'SU'
+
+DAYS_OF_WEEK = (
+    (MONDAY, 'Monday'),
+    (TUESDAY, 'Tuesday'),
+    (WEDNESDAY, 'Wednesday'),
+    (THURSDAY, 'Thursday'),
+    (FRIDAY, 'Friday'),
+    (SATURDAY, 'Saturday'),
+    (SUNDAY, 'Sunday'))
+
+# This is abstract form used for creating schedule for each module.
 class ScheduleForm(forms.Form):
-    MONDAY = 'M'
-    TUESDAY = 'TU'
-    WEDNESDAY = 'W'
-    THURSDAY = 'TH'
-    FRIDAY = 'F'
-    SATURDAY = 'SA'
-    SUNDAY = 'SU'
-
-    DAYS_OF_WEEK = (
-        (MONDAY, 'Monday'),
-        (TUESDAY, 'Tuesday'),
-        (WEDNESDAY, 'Wednesday'),
-        (THURSDAY, 'Thursday'),
-        (FRIDAY, 'Friday'),
-        (SATURDAY, 'Saturday'),
-        (SUNDAY, 'Sunday'))
-
     start_time = forms.TimeField()
     start_day = forms.ChoiceField(choices = DAYS_OF_WEEK)
     end_time = forms.TimeField()
     end_day = forms.ChoiceField(choices = DAYS_OF_WEEK)
-    status = None  # to be overriten in child models
+    status = None  # to be overriten in child forms
 
-    # form_fields = [ 'start_day', 'start_time', 'end_day', 'end_time', 'status' ]
+    def __repr__(self):
+        return self._get_js_format()
 
-    # def __repr__(self):
-    #     return self._get_js_format()
+    def _get_js_format(self):
+        return """{ start_time : \'%s\',
+            start_day : \'%s\',
+            end_time : \'%s\',
+            end_day : \'%s\' }""" % (self.start_time, self.start_day,
+                self.end_time, self.end_day)
 
-    # def _get_js_format(self):
-    #     return """{ start_time : \'%s\',
-    #         start_day : \'%s\',
-    #         end_time : \'%s\',
-    #         end_day : \'%s\' }""" % (self.start_time, self.start_day,
-    #             self.end_time, self.end_day)
+class Task:
+    def __init__(self, id, dbus_task):
+        self.id = id
+        self.start_day = Task._get_readable_day(dbus_task[0])
+        self.start_time = time(dbus_task[1][0], dbus_task[1][1])
+        self.end_day = Task._get_readable_day(dbus_task[2])
+        self.end_time = time(dbus_task[3][0], dbus_task[3][1])
+        self.status = dbus_task[4]
+
+    @staticmethod
+    def _get_readable_day(raw_day):
+        return next(x[1] for x in DAYS_OF_WEEK if x[0] == raw_day)
 
 class ScheduleDbusInterface:
     @staticmethod
